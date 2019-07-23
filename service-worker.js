78bd59b1f429152f9cc637aea7e3f2ef983cdb2d
Pavly4ek <53210153+Pavly4ek@users.noreply.github.com>
2019-07-23 18:23:05 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.001dab2a5bb5b60cb39f539d609a7d97.js"
+  "/precache-manifest.67de0e65a97bb63581ef8c9f4bfc8780.js"
 );
 
 self.addEventListener('message', (event) => {
